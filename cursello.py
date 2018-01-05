4a75e3d7bd894a39cd91754825f425e47f5dd7cf
David H <dhdavvie@gmail.com>
2018-01-05 18:39:05 +0100

    change io. added saving and switching

--- cursello.py
+++ cursello.py
@@ -16,6 +16,8 @@ curses.start_color()
 curses.curs_set(0)
 stdscr.refresh()
 
+changes_made = False
+
 if len(sys.argv) == 2:
   # User has given board name
   board_name = sys.argv[1]
@@ -45,17 +47,23 @@ def refresh_lists(stdscr, data, ilist, itemw):
 
 
 def new_item(stdscr, data, ilist):
+  global changes_made
+  changes_made = True
   item_string = BarInput(stdscr, "Enter the item description, then press enter: ")
   data[ilist].add(item_string)
 
 
 def new_list(stdscr, data, ilist):
+  global changes_made
+  changes_made = True
   item_string = BarInput(stdscr, "Enter the name of the new list, then press enter: ")
   data.append(List(item_string))
   ilist = len(data) - 1
 
 
 def delete_item(stdscr, data, ilist, item):
+  global changes_made
+  changes_made = True
   if ilist >= len(data) or ilist < 0 or item >= data[ilist].size or item < 0:
     return
   answer = BarInput(stdscr, "Are you sure you wish to delete this item? (y/n): ")
@@ -64,6 +72,8 @@ def delete_item(stdscr, data, ilist, item):
 
 
 def delete_list(stdscr, data, ilist):
+  global changes_made
+  changes_made = True
   if ilist >= len(data) or ilist < 0:
     return
   answer = BarInput(stdscr, "Are you sure you wish to delete this entire list? (y/n): ")
@@ -72,13 +82,42 @@ def delete_list(stdscr, data, ilist):
 
 
 def save_board(stdscr, data):
-  board_name = BarInput(stdscr, 'What name do you want to save this board as? Leave blank to save as: ' + storage.STORAGE_FILE.strip('.yaml'))
+  global changes_made
+  board_name = BarInput(stdscr, 'What name do you want to save this board as? Leave blank to save as "' + storage.STORAGE_FILE.strip('.yaml') + '": ')
+
+  if not board_name == '':
+    if '.yaml' not in board_name:
+      board_name += '.yaml'
+
+    storage.STORAGE_FILE = board_name
+
+  storage.write_store(data)
+
+  changes_made = False
+
+
+def switch_board(stdscr, data):
+  global changes_made
+
+  if changes_made:
+    # User has unsaved changes, prompt to see if they wish to save
+    answer = BarInput(stdscr, 'You have unsaved changes in the current board, do you wish to save them? (y/n): ')
+    if answer == 'y':
+      save_board(stdscr, data)
+
+  board_name = BarInput(stdscr, 'Which board do you wish to switch to: ')
 
   if '.yaml' not in board_name:
     board_name += '.yaml'
 
   storage.STORAGE_FILE = board_name
-  storage.write_store(data)
+
+  storage_file_present = storage.check_store()
+  data = storage.load_store()
+  if data is None or len(data) == 0:
+    data = [List("To Do"), List("Done")]
+
+  return data
 
 
 def init(stdscr):
@@ -129,6 +168,11 @@ def main(stdscr):
     # Save board
     elif c == ord('w'):
       save_board(stdscr, data)
+    # Switch boards
+    elif c == ord('m'):
+      data = switch_board(stdscr, data)
+      ilist = 0
+      item = 0
     # Quit
     elif c == ord('q'):
       break
